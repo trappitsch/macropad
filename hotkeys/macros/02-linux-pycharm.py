afead77afb2bb737aeace7f2bfa361c840adca98
Reto Trappitsch <reto@galactic-forensics.space>
2022-08-19 10:39:56 +0200

    Split PyCharm Layout out: editing, debugging

--- hotkeys/macros/02-linux-pycharm.py
+++ hotkeys/macros/02-linux-pycharm.py
@@ -15,16 +15,13 @@ app = {  # REQUIRED dict, must be named 'app'
         (0x400000, "RunMenu", [Keycode.ALT, Keycode.SHIFT, Keycode.F10]),
         (0x000040, "Tab >", [Keycode.CONTROL, Keycode.PAGE_DOWN]),
         # 3rd row ----------,
-        (0x400000, "BrkPt", [Keycode.CONTROL, Keycode.F8]),
-        (0x462000, "StpOv", [Keycode.F8]),
-        (0x462000, "StpOut", [Keycode.SHIFT, Keycode.F8]),
+        (0x280046, "Usage", [Keycode.ALT, Keycode.SHIFT, "7"]),
+        (0x280046, "Decl", [Keycode.CONTROL, "b"]),
+        (0x462000, "Rename", [Keycode.SHIFT, Keycode.F6]),
         # 4th row ----------
-        # (0x202000, "Proj", [Keycode.ALT, "1"]),
-        # (0x202000, "Struct", [Keycode.ALT, "7"]),
-        # (0x202000, "Struct", [Keycode.bALT, "7"]),
-        (0x280046, "StpMy", [Keycode.ALT, Keycode.SHIFT, Keycode.F7]),
-        (0x280046, "RunCur", [Keycode.ALT, Keycode.SHIFT, "9"]),
-        (0x004000, "Res", [Keycode.F9]),
+        (0x000000, "Black", [Keycode.ALT, Keycode.SHIFT, "B"]),
+        (0x202000, "Struct", [Keycode.ALT, "7"]),
+        (0x101010, "Sett", [Keycode.CONTROL, Keycode.ALT, "s"]),
         # Encoder button ---
         (0x000000, "", [Keycode.CONTROL, "w"]),  # Close window/tab
     ],
---
+++ hotkeys/macros/03-linux-pycharm-debug.py
@@ -0,0 +1,28 @@
+# MACROPAD Hotkeys example: Firefox web browser for Linux
+
+from adafruit_hid.keycode import Keycode  # REQUIRED if using Keycode.* values
+
+app = {  # REQUIRED dict, must be named 'app'
+    "name": "PyCharm - Debug",  # Application name
+    "macros": [  # List of button macros...
+        # COLOR    LABEL    KEY SEQUENCE
+        # 1st row ----------
+        (0x400000, "Debug", [Keycode.SHIFT, Keycode.F9]),
+        (0x004000, "Run", [Keycode.SHIFT, Keycode.F10]),
+        (0x004000, "RunThis", [Keycode.CONTROL, Keycode.SHIFT, Keycode.F10]),
+        # 2nd row ----------
+        (0x400000, "BrkPt", [Keycode.CONTROL, Keycode.F8]),
+        (0x101010, "ShowEx", [Keycode.ALT, Keycode.SHIFT, "0"]),
+        (0x004000, "Res", [Keycode.F9]),
+        # 3rd row ----------
+        (0x280046, "StpIn", [Keycode.F7]),
+        (0x280046, "StpMy", [Keycode.ALT, Keycode.SHIFT, Keycode.F7]),
+        (0x280046, "RunCur", [Keycode.ALT, Keycode.SHIFT, "9"]),
+        # 4th row ----------
+        (0x462000, "StpOv", [Keycode.F8]),
+        (0x462000, "StpOut", [Keycode.SHIFT, Keycode.F8]),
+        (0x000040, "Eval", [Keycode.ALT, Keycode.SHIFT, "8"]),
+        # Encoder button ---
+        (0x000040, "", [Keycode.CONTROL, "w"]),  # Close window/tab
+    ],
+}
