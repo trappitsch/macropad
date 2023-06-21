480b65d86826f969522d5424845a18226f1fdaba
Reto Trappitsch <reto@galactic-forensics.space>
2023-06-21 16:21:30 +0200

    Update to configs: VSCode

---
+++ hotkeys/macros/01-vscode.py
@@ -0,0 +1,28 @@
+# MACROPAD Hotkeys example: Firefox web browser for Linux
+
+from adafruit_hid.keycode import Keycode  # REQUIRED if using Keycode.* values
+
+app = {  # REQUIRED dict, must be named 'app'
+    "name": "VSCode",  # Application name
+    "macros": [  # List of button macros...
+        # COLOR    LABEL    KEY SEQUENCE
+        # 1st row ----------
+        (0x400000, "Debug", [Keycode.F5]),
+        (0x004000, "Run", [Keycode.CONTROL, Keycode.F5]),  # todo
+        (0x004000, "RunThis", [Keycode.CONTROL, Keycode.F5]),
+        # 2nd row ----------
+        (0x000040, "< Tab", [Keycode.CONTROL, Keycode.PAGE_UP]),
+        (0x462000, "Refract", [Keycode.CONTROL, Keycode.SHIFT, "R"]),
+        (0x000040, "Tab >", [Keycode.CONTROL, Keycode.PAGE_DOWN]),
+        # 3rd row ----------,
+        (0x280046, "Refs", [Keycode.SHIFT, Keycode.F12]),
+        (0x280046, "Decl", [Keycode.F12]),
+        (0x462000, "Rename", [Keycode.F2]),
+        # 4th row ----------
+        (0x000000, "Black", [Keycode.CONTROL, Keycode.SHIFT, "I"]),
+        (0x202000, "Struct", [Keycode.ALT, Keycode.SHIFT, "7"]),
+        (0x101010, "Files", [Keycode.CONTROL, Keycode.SHIFT, "E"]),
+        # Encoder button ---
+        (0x000000, "", [Keycode.CONTROL, "W"]),  # Close window/tab
+    ],
+}
---
+++ hotkeys/macros/02-vscode-debug.py
@@ -0,0 +1,28 @@
+# MACROPAD Hotkeys example: Firefox web browser for Linux
+
+from adafruit_hid.keycode import Keycode  # REQUIRED if using Keycode.* values
+
+app = {  # REQUIRED dict, must be named 'app'
+    "name": "VSCode Debug",  # Application name
+    "macros": [  # List of button macros...
+        # COLOR    LABEL    KEY SEQUENCE
+        # 1st row ----------
+        (0x400000, "Debug", [Keycode.F5]),
+        (0x004000, "Run", [Keycode.CONTROL, Keycode.F5]),  # todo
+        (0x004000, "RunThis", [Keycode.CONTROL, Keycode.F5]),
+        # 2nd row ----------
+        (0xff5900, "Stop", [Keycode.F9]),
+        (0xff5900, "Restart", [Keycode.ALT, Keycode.SHIFT, "0"]),
+        (0x000040, "Console", [Keycode.CONTROL, Keycode.SHIFT, "Y"]),
+        # 3rd row ----------
+        (0x280046, "StpOv", [Keycode.F10]),
+        (0x280046, "StpIn", [Keycode.F11]),
+        (0x280046, "StpOut", [Keycode.SHIFT, Keycode.F11]),
+        # 4th row ----------
+        (0x400000, "BrkPt", [Keycode.F9]),
+        (0x400000, "CondBrk", [Keycode.CONTROL, Keycode.SHIFT, Keycode.F9]),
+        (0x400000, "InlBrk", [Keycode.SHIFT, Keycode.F9]),
+        # Encoder button ---
+        (0x000000, "", [Keycode.CONTROL, "W"]),  # Close window/tab
+    ],
+}
